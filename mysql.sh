2f145b2b7eda5f2939ec993b6dd4d2e5bdf595a3
mahender <gmahe139@gmail.com>
2025-01-06 06:25:08 +0530

    mysql script added

---
+++ mysql.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+USERID=$(id -u) # Display UserID value, For root user its 0
+R="\e[31m" # Print Red color
+G="\e[32m" # Print Green color
+Y="\e[33m" # Print Yellow color
+N="\e[0m" # Print Default White color
+LOGS_FOLDER="/var/log/Expenseshelllogs"
+LOG_FILE=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
+
+mkdir -p $LOGS_FOLDER
+echo "Created the $LOGS_FOLDER directory"
+echo "Started the shell script at $TIMESTAMP" &>>$LOG_FILE_NAME
+
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR : User does not have SUDO access to execute this script"
+    exit 1 # Other than 0
+fi
+
+
+VALIDATE()
+{
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2..$R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2..$G SUCCESS $N"
+    fi
+}
+
+
+dnf list installed mysql-server &>>$LOG_FILE_NAME
+
+if [ $? -ne 0 ]
+then
+    echo "MYSQL Server is not installed..So, Installing MYSQL Server"
+    dnf install mysql-server -y &>>$LOG_FILE_NAME
+    VALIDATE $? "MYSQL Server Installation"
+else
+    echo -e "$Y MYSQL Server is already Installed $N"
+fi
+
+systemctl enable mysqld &>>$LOG_FILE_NAME
+systemctl restart mysqld &>>$LOG_FILE_NAME
+mysql_secure_installation --set-root-pass ExpenseApp@1 
